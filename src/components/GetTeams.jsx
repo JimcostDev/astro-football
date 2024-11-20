1d67097e3802dfe8ef91586eab086abb0fc0427e
JimcostDev <rjak727@gmail.com>
2024-11-20 14:52:41 +0100

    add boton para agregar equipo y mejora en la carga de equipos

--- src/components/GetTeams.jsx
+++ src/components/GetTeams.jsx
@@ -1,34 +1,37 @@
 import { useEffect, useState } from 'react';
-import { getAllTeamsOrGetTeamsByLeague } from '../api/loadData.js';
-import CardTeams from './CardTeams.jsx'; // Asegúrate de que este componente también sea un componente de React.
+import { getAllTeamsOrGetTeamsByLeague } from '../services/loadData.js';
+import CardTeams from './CardTeams.jsx';
 
 const GetTeams = () => {
-  const [getTeams, setGetTeams] = useState([]);
-  const [leagueName, setLeagueName] = useState('');
+  const [getTeams, setGetTeams] = useState([]);  // Estado para los equipos
+  const [leagueName, setLeagueName] = useState('');  // Estado para la liga seleccionada
+  const [loading, setLoading] = useState(true);  // Estado para saber si estamos cargando los datos
 
+  // UseEffect para cargar equipos cuando la liga cambie o cuando se cargue el componente
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
-    const league = searchParams.get('league_name') || '';
+    const league = searchParams.get('league_name') || '';  // Obtener la liga desde los parámetros de la URL
     setLeagueName(league);
 
     const fetchTeams = async () => {
+      setLoading(true);  // Empezamos a cargar los datos
       const teamData = await getAllTeamsOrGetTeamsByLeague(league);
-      setGetTeams(Array.isArray(teamData) ? teamData : []);
+      setGetTeams(Array.isArray(teamData) ? teamData : []);  // Establecer los equipos recibidos
+      setLoading(false);  // Terminamos de cargar los datos
     };
 
     fetchTeams();
-  }, []);
+  }, []);  // Solo se ejecuta una vez al montar el componente
 
   return (
     <div>
       <div className="container mx-auto flex justify-between items-center my-3">
-        <a href="/" className="text-blue-800 text-3xl font-bold">JimcostDev Football</a>
         <nav>
           <form className="flex items-center" action="/" method="GET">
             <select
               id="league-filter"
               name="league_name"
-              className="border border-gray-300 rounded-md px-4 py-2 mr-2"
+              className="border border-blue-400 rounded-md px-4 py-2 mr-2"
               value={leagueName}
               onChange={(e) => setLeagueName(e.target.value)}
             >
@@ -41,7 +44,7 @@ const GetTeams = () => {
             </select>
             <button
               type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="bg-transparent border border-blue-400 hover:bg-blue-500/10 py-2 px-4 rounded-lg shadow-md hover:shadow-xl ease-in-out duration-500 cursor-pointer"
             >
               Buscar
             </button>
@@ -50,20 +53,25 @@ const GetTeams = () => {
       </div>
 
       {leagueName && (
-        <p className="text-green-500 font-medium text-lg my-2">Liga: {leagueName}</p>
+        <p className="text-green-500 font-medium text-lg my-2">{leagueName}</p>
       )}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {getTeams.length > 0 ? (
-          getTeams.map(({ name, league, country }) => (
-            <CardTeams key={name} name={name} league={league} country={country} />
-          ))
-        ) : (
-          <p className="mx-5 text-wrap text-center font-light text-red-500/90">
-            No se encontraron equipos para la liga seleccionada.
-          </p>
-        )}
-      </div>
+      {/* Mostrar mensaje de carga mientras se obtienen los equipos */}
+      {loading ? (
+        <p className="text-center text-lg text-blue-500">Cargando equipos...</p>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {getTeams.length > 0 ? (
+            getTeams.map(({ name, league, country }) => (
+              <CardTeams key={name} name={name} league={league} country={country} />
+            ))
+          ) : (
+            <p className="mx-5 text-wrap text-center font-light text-red-500/90">
+              No se encontraron equipos para la liga seleccionada.
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/services/loadData.js
@@ -0,0 +1,113 @@
+// URL base de la API
+const API_URL = "https://fastapi-football.koyeb.app";
+
+// Obtener todos los equipos o equipos por liga
+export async function getAllTeamsOrGetTeamsByLeague(league = '') {
+    let urlAPI = `${API_URL}/`;
+
+    // Si se proporciona un nombre de liga, lo añadimos a la URL
+    if (league) {
+        urlAPI += `?league_name=${league}`;
+    }
+
+    try {
+        const response = await fetch(urlAPI);
+        if (!response.ok) {
+            throw new Error(`No se pudo obtener la respuesta de la API, status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error al obtener los datos de la API, ${error}`);
+        return null;
+    }
+}
+
+// Obtener un equipo por ID
+export async function getTeamById(teamId) {
+    const urlAPI = `${API_URL}/${teamId}`;
+    try {
+        const response = await fetch(urlAPI);
+        if (!response.ok) {
+            throw new Error(`No se pudo obtener el equipo, status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error al obtener el equipo, ${error}`);
+        return null;
+    }
+}
+
+// Crear un nuevo equipo
+export async function createTeam(teamData) {
+    const urlAPI = `${API_URL}/`;
+
+    try {
+        const response = await fetch(urlAPI, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(teamData),
+        });
+
+        if (!response.ok) {
+            throw new Error(`No se pudo crear el equipo, status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error al crear el equipo, ${error}`);
+        return null;
+    }
+}
+
+// Actualizar un equipo
+export async function updateTeam(teamId, teamData) {
+    const urlAPI = `${API_URL}/${teamId}`;
+
+    try {
+        const response = await fetch(urlAPI, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(teamData),
+        });
+
+        if (!response.ok) {
+            throw new Error(`No se pudo actualizar el equipo, status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error al actualizar el equipo, ${error}`);
+        return null;
+    }
+}
+
+// Eliminar un equipo
+export async function deleteTeam(teamId) {
+    const urlAPI = `${API_URL}/${teamId}`;
+
+    try {
+        const response = await fetch(urlAPI, {
+            method: 'DELETE',
+        });
+
+        if (!response.ok) {
+            throw new Error(`No se pudo eliminar el equipo, status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error al eliminar el equipo, ${error}`);
+        return null;
+    }
+}
